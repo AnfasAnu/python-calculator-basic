4d212a6b1041a5eb66d455ecbe9398d9e0135692
Anfas <179591027+AnfasAnu@users.noreply.github.com>
2026-05-21 02:38:07 +0530

    day 1

--- calculator.py
+++ calculator.py
@@ -1,1 +1,28 @@
-print("Calculator Started")
+while True:
+   
+    operator = input("Enter operation (+, -, *, /,!): ")
+    if operator =="!":
+        print("Calculator Closed")
+        break
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    
+    if operator == "+":
+        print("Result:", num1 + num2)
+
+    elif operator == "-":
+        print("Result:", num1 - num2)
+
+    elif operator == "*":
+        print("Result:", num1 * num2)
+
+    elif operator == "/":
+        if num2 == 0:
+            print("Error: Division by zero")
+        else:
+            print("Result:", num1 / num2)
+ 
+
+    else:
+        print("Invalid operator")
